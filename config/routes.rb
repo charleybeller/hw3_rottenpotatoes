99a5a42954598dbf83a4e83d9744f733c8f578c4
Ubuntu <ubuntu@ip-10-160-247-153.us-west-1.compute.internal>
2012-10-24 04:13:28 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
+  #Route that posts 'Search TMDb' form
+  post '/movies/search_tmdb'
+
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    new_movie = Movie.create(movie)
   end
-  flunk "Unimplemented"
+#  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+#  flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
