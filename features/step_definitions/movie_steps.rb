17916ad2aa0430623f8ecfaebfe90384b06d3429
Ubuntu <ubuntu@ip-10-160-247-153.us-west-1.compute.internal>
2012-10-24 22:35:45 +0000

    init

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -15,8 +15,29 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-#  flunk "Unimplemented"
+  flunk "Unimplemented"
 end
+
+Then /I should see movies titled (.+)$/ do |titles|
+  titles.split(', ').each do |title|
+    if page.respond_to? :should
+      page.should have_content(title)
+    else
+      assert page.has_content?(title)
+    end
+  end
+end
+
+Then /I should not see movies titled (.+)$/ do |titles|
+  titles.split(', ').each do |title|
+    if page.respond_to? :should
+      page.should have_no_content(title)
+    else
+      assert page.has_no_content?(title)
+    end
+  end
+end
+
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
@@ -26,4 +47,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(', ').each do |rating|
+    box = find("#ratings_#{rating}")
+    if uncheck
+      box.set(false)
+    else
+      box.set(true)
+    end
+  end
+end
+
+When /^I click the "([^"]+)" button/ do |element|
+  page.find("##{element}").click
 end
